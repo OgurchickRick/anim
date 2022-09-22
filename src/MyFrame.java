7e60f6e2249fde4abc8aa3dd589356602df3dd00
OgurchickRick <artem.derbin.04@list.ru>
2022-09-23 00:04:02 +0300

    Из угла в угол

---
+++ src/MyFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MyFrame extends Jframe{
+    JFrame frame = new JFrame("Окно");
+
+        frame.add();
+
+        frame.setPreferredSize(new Dimension(720, 720));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+
+        frame.setVisible(true);
+}
