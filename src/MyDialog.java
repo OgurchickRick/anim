aca4952832be6fedbb289e0db5c7e79a3567782f
OgurchickRick <artem.derbin.04@list.ru>
2022-10-09 22:47:06 +0300

    Диалоговое окно (вопросы)

--- src/MyDialog.java
+++ src/MyDialog.java
@@ -3,12 +3,20 @@ import java.io.IOException;
 
 public class MyDialog {
 
-    String name;
-
     MyFrame f;
     MyDialog() throws IOException {
         f = new MyFrame();
-        name = JOptionPane.showInputDialog(f, "Введите ваше имя");
-        JOptionPane.showMessageDialog(f, name);
+        int a = JOptionPane.showConfirmDialog(f, "Ты тупой?");
+        int b = JOptionPane.showConfirmDialog(f, "Уверен?");
+        if (a == 0 && b == 0){
+            JOptionPane.showMessageDialog(f, "Я же говорил");
+        }else if (a == 0 && b == 1){
+            JOptionPane.showMessageDialog(f, "Определился бы уже");
+        }else if (a == 1 && b == 0){
+            JOptionPane.showMessageDialog(f, "Какой самоуверенный");
+        }else if (a == 1 && b == 1){
+            JOptionPane.showMessageDialog(f, "Биполярка?");
+        }
+
     }
 }
