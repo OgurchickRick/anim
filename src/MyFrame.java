192451bf7ac42192302d0824eb0ca59b3030b727
OgurchickRick <artem.derbin.04@list.ru>
2022-10-11 23:32:46 +0300

    Диалоговое окно (пароль)

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,18 +1,73 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class MyFrame extends JFrame{
     MyPanel panel;
+    String login = " ";
+    String password1;
+    String password2;
 
-    MyFrame() throws IOException {
+    MyFrame() {
         panel = new MyPanel();
 
         this.setTitle("Game");
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.add(panel);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(500, 300));
         this.pack();
-        this.setLocation(700, 400);
+
+        this.setLocation(600, 300);
         this.setVisible(true);
+
+        int otw = JOptionPane.showConfirmDialog(null, "Хотитете зарегистрироваться?");
+        System.out.println(otw);
+        if (otw == 0){
+            while (true){
+                login = JOptionPane.showInputDialog(null, "Введите логин", "Username");
+                if (login == null){
+                    this.dispose();
+                    break;
+                }
+                if (login.length() > 5 && !login.contains(" ")){
+                    break;
+                }
+            }
+            while (true){
+                password1 = JOptionPane.showInputDialog(null, "Введите пароль");
+                if (password1 == null){
+                    this.dispose();
+                    break;
+                }
+                if (password1.toString().length() > 8){
+                    char[] mas = password1.toString().toCharArray();
+                    boolean isD = false;
+                    boolean isC = false;
+                    for (char c:mas){
+                        if (Character.isDigit(c)){
+                            isD = true;
+                        }
+                        if (Character.isAlphabetic(c)){
+                            isC = true;
+                        }
+                    }
+                    if (isC && isD){
+                        break;
+                    }
+                }
+            }
+            while (true){
+                password2 = JOptionPane.showInputDialog(null, "Подтвердите пароль");
+                if (password2 == null){
+                    this.dispose();
+                    break;
+                }
+                if (password1.equals(password2)){
+                    break;
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Добро пожаловать " + login);
+        }else {
+            this.dispose();
+        }
     }
 }
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,17 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.IOException;
 
 public class MyPanel extends JPanel {
 
-    String otwet;
 
-    String[] var = {"800x600", "1024x768", "1920x1080"};
+    MyPanel() {
 
-    MyPanel(){
-        otwet = (String)JOptionPane.showInputDialog(null, "Выберите разрешение", "Выбор разрешения", JOptionPane.QUESTION_MESSAGE, null, var, var[0]);
-        this.setPreferredSize(new Dimension(Integer.parseInt(otwet.split("x")[0]), Integer.parseInt(otwet.split("x")[1])));
     }
 
 
