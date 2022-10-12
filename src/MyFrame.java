54f49906be04e3cd9ed5a50b81178a42247d96d0
OgurchickRick <artem.derbin.04@list.ru>
2022-10-12 23:08:12 +0300

    JSlider

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -3,24 +3,13 @@ import java.awt.*;
 
 public class MyFrame extends JFrame{
     MyPanel panel;
-    String login = " ";
-    String password1;
-    String password2;
-    Font font = new Font("Verdana", Font.ITALIC, 50);
-    JLabel label;
-
-
 
     MyFrame() {
         panel = new MyPanel();
-        label = new JLabel("Моя первая надпись!");
-        label.setFont(font);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
         this.setTitle("Game");
-        this.add(label);
+        this.add(panel);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(1000, 500));
+
         this.pack();
 
         this.setLocation(100, 50);
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,13 +1,36 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.io.IOException;
 
-public class MyPanel extends JPanel {
+public class MyPanel extends JPanel{
 
+    Font font;
+    JLabel label;
+    JSlider slider;
+
+    String FontFam = "Verdana";
+    int i = Font.ITALIC;
 
     MyPanel() {
-
+        this.setPreferredSize(new Dimension(1000, 500));
+        slider = new JSlider(5, 100, 50);
+        slider.setPaintLabels(true);
+        slider.setMajorTickSpacing(10);
+        label = new JLabel("Моя первая надпись!");
+        font = new Font(FontFam, i, slider.getValue());
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setFont(font);
+        slider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                // меняем надпись
+                int value = ((JSlider)e.getSource()).getValue();
+                font = new Font(FontFam, i, value);
+                label.setFont(font);
+            }
+        });
+        this.add(slider);
+        this.add(label);
     }
-
-
 }
