4cb47d036793c042a5ffffe24daa18e703b36566
OgurchickRick <artem.derbin.04@list.ru>
2022-10-13 11:38:18 +0300

    JRadioButton

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,37 +10,44 @@ public class MyPanel extends JPanel{
     JLabel label;
 
     JButton but;
-
-    JComboBox <String> combo;
-    JCheckBox check;
-
-    JTextField input;
-
-    String[] colors = {"Красный", "Черный", "Белый", "Синий", "Зелёный"};
+    JRadioButton rb1, rb2, rb3, rb4;
 
     MyPanel() {
         this.setPreferredSize(new Dimension(100, 300));
         label = new JLabel("Ответ: ");
         but = new JButton("Ответить");
-        combo = new JComboBox<String>(colors);
-        check = new JCheckBox("Свой вариант");
-        input = new JTextField("Placeholder");
+        rb1 = new JRadioButton("Весна");
+        rb2 = new JRadioButton("Лето");
+        rb3 = new JRadioButton("Осень");
+        rb4 = new JRadioButton("Зима");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(rb1);
+        bg.add(rb2);
+        bg.add(rb3);
+        bg.add(rb4);
+        bg.add(but);
+
 
 
         but.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (check.isSelected()){
-                    value = input.getText();
-                }else {
-                    value = combo.getItemAt(combo.getSelectedIndex());
+                if (rb1.isSelected()){
+                    value = rb1.getText();
+                }else if (rb2.isSelected()){
+                    value = rb2.getText();
+                }else if (rb3.isSelected()){
+                    value = rb3.getText();
+                }else if (rb4.isSelected()){
+                    value = rb4.getText();
                 }
                 label.setText("Ответ: " + value);
             }
         });
-        this.add(combo);
-        this.add(check);
-        this.add(input);
+        this.add(rb1);
+        this.add(rb2);
+        this.add(rb3);
+        this.add(rb4);
         this.add(but);
         this.add(label);
     }
